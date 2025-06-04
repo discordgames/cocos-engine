d55f0130dbe489f489ae7a9d71f7f09f1e243f98
Callabrator <dave.calla@discordapp.com>
2025-06-04 11:40:18 -0500

    put formatting back the way it was

--- cocos/core/scheduler.ts
+++ cocos/core/scheduler.ts
@@ -23,11 +23,11 @@
  THE SOFTWARE.
 */
 
-import { legacyCC } from './global-exports';
-import { assertID, errorID, logID, warnID } from './platform/debug';
-import { System } from './system';
 import { IDGenerator } from './utils/id-generator';
 import { createMap } from './utils/js';
+import { System } from './system';
+import { legacyCC } from './global-exports';
+import { errorID, warnID, logID, assertID } from './platform/debug';
 
 const MAX_POOL_SIZE = 20;
 
